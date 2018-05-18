9151e807616d6d9197dc7f9278d119f7dc363c2f
Ruth Waiganjo <ruthwaiganjo@gmail.com>
2018-05-18 15:21:38 +0300

    simple unit test file added

--- application/even_numbers.py
+++ application/even_numbers.py
@@ -1,12 +1,16 @@
 #checks for even numbers
 
 def number(num):
+
+    #check if the number is an integer
     if type(num) != int:
         raise ValueError("Number must be an integer")
 
+    #test if the number is positive
     if num < 1:
         raise ValueError("Must enter a positive number")
 
+    #test if the number is even
     if num % 2 == 0:
             return True
     
---
+++ test/test_even_numbers.py
@@ -0,0 +1,22 @@
+import unittest
+from application import even_numbers
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_even(self):
+        self.assertTrue(even_numbers.number(2))
+
+    def test_not_even(self):
+        self.assertFalse(even_numbers.number(3))    
+
+    def test_if_number_is_integer(self):
+        with self.assertRaises(ValueError):
+            even_numbers.number("one")  
+
+    def test_if_number_is_positive(self):
+        with self.assertRaises(ValueError):
+            even_numbers.number(-1)          
+
+
+if __name__ == '__main__':
+    unittest.main()        
